d037ee8ee9e1e339191b4422859f18d62a366a1b
harinikumari2023 <harini.kattari.2023@gmail.com>
2025-05-26 14:29:31 +0530

    Update config to use Render PostgreSQL

--- instance/config.py
+++ instance/config.py
@@ -4,6 +4,6 @@ Created on Fri May 23 15:43:14 2025
 
 @author: USER
 """
-
-SQLALCHEMY_DATABASE_URI = 'sqlite:///funds.db'
+#SQLALCHEMY_DATABASE_URI = 'sqlite:///funds.db'
+SQLALCHEMY_DATABASE_URI = 'postgresql://sportsfund_user:Ilji6IToyE79ikIqcpzljk2v7F7VD9DA@dpg-d0q2m0muk2gs73a64ihg-a.oregon-postgres.render.com/sportsfund_db'
 SQLALCHEMY_TRACK_MODIFICATIONS = False
